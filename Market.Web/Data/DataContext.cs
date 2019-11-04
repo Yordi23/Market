b66c40e88902344f755ecf5abe6060220600cd50
Yordi Ogando <yordiogando@gmail.com>
2019-11-04 13:10:05 -0400

    Products controller added

---
+++ Market.Web/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Market.Web.Data
+{
+    using Market.Web.Data.Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+    }
+}
